228bfb0c1438eb84f3e2ea66352e4f0cdc291cce
sahwaz768 <sahwazalam768@gmail.com>
2024-11-03 19:54:36 +0530

    this is the third commit

--- src/app/user/chat/page.jsx
+++ src/app/user/chat/page.jsx
@@ -1,16 +1,16 @@
 'use client'
 import React from "react";
-import Header from "@/components/Header";
+import { RiChatSmile3Line } from "react-icons/ri";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdManageHistory } from "react-icons/md";
+import { BiLogOutCircle } from "react-icons/bi";
 import Image from "next/image";
 import Profile from "@/app/homepageimg.jpg";
-import { RiChatSmile3Line } from "react-icons/ri";
-import { FaHistory } from "react-icons/fa";
-import { BiLogOutCircle } from "react-icons/bi";
-import { CgProfile } from "react-icons/cg";
+import { IoIosNotificationsOutline } from "react-icons/io";
+
 import Chatwindow from "@/components/chatwindow";
 
 const page = () => {
-
   const handleResize = () => {
     if (window.matchMedia("(min-width: 768px)").matches) {
       showchat1();
@@ -20,100 +20,93 @@ const page = () => {
   };
   
   const showchat1 = () => {
-    document.getElementById('chatcomponentwindow').style.display = 'block';
+    document.getElementById('chatwindow').style.display = 'block';
   };
   
   const showchat = () => {
     document.getElementById('chatlist').style.display = 'none';
-    document.getElementById('chatcomponentwindow').style.display = 'block';
+    document.getElementById('chatwindow').style.display = 'block';
   };
-
- 
-
   return (
-   
-    <div>
-      <Header />
-      <div className="chat-top-box">
-        <div className="mb-view" id="chatlist">
-        <div className="sidebartop">
-          <div>
-            <nav>
-              <ul className="sidebar">
-                <li className="sidebarprofiletop">
-                  <Image src={Profile} alt="profile"/>
+  <>
+<h1 className="chatzestful">zestful amigos</h1>
+ <div className="chatpage">
+      <nav>
+        <ul className="sidebar">
+          <li className="menu-item">
+            <span className="icon">
+              <IoHomeOutline color="black" size={20} />
+            </span>
+            <span className="label">Home</span>
+          </li>
+          <li className="menu-item">
+            <span className="icon">
+              <RiChatSmile3Line color="black" size={20} />
+            </span>
+            <span className="label">Chat</span>
+          </li>
+          <li className="menu-item">
+            <span className="icon">
+              <MdManageHistory color="black" size={20} />
+            </span>
+            <span className="label">booking history</span>
+          </li>
+          <li className="menu-item">
+            <span className="icon">
+              <BiLogOutCircle color="black" size={20} />
+            </span>
+            <span className="label">logout</span>
+          </li>
+          <li className="sidebarprofiletop">
+            <Image src={Profile} alt="profile" className="last-profile" />
 
-                  <div className="ml-4 sidebarprofile">
-                    <h1 className="font-extrabold text-lg">Olivia Rhye</h1>
-                    <p className="text-xs">olivia@gmail.com</p>
-                  </div>
-                </li>
-                <li className="menu-item">
-                  <span className="icon">
-                    <CgProfile color="black" size={20} />
-                  </span>
-                  <span className="label text-sm">profile setting</span>
-                </li>
-                <li className="menu-item">
-                  <span className="icon">
-                    <RiChatSmile3Line color="black" size={20} />
-                  </span>
-                  <span className="label text-sm">Chat</span>
-                </li>
-                <li className="menu-item">
-                  <span className="icon">
-                    <FaHistory color="black" size={20} />
-                  </span>
-                  <span className="label text-sm">Booking history</span>
-                </li>
-
-                <li className="menu-item">
-                  <span className="icon">
-                    <BiLogOutCircle color="black" size={20} />
-                  </span>
-                  <span className="label text-sm">Logout</span>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        
-        <div className="chatlist" >
-          <h1 className="text-sm font-bold mt-5 mb-2"> Chats</h1>
-          <div className="userlist">
-            <div className="userdetail flex items-center " onClick={handleResize}>
-              <div>
-                <Image src={Profile} alt="Picture of the user" />
-              </div>
-              <div>
-                <h1 className="text-sm ml-3">Olivia</h1>
-              </div>
-              <div className="userstatus">
-                <h1 className="text-xs ">today</h1>
-              </div>
+            <div className=" sidebarprofile ml-4 flex">
+              <h1 className="label">Olivia Rhye</h1>
             </div>
-            <div className="userdetail flex items-center ">
-              <div>
-                <Image src={Profile} alt="Picture of the user" />
-              </div>
-              <div>
-                <h1 className="text-sm ml-3">Olivia</h1>
-              </div>
-              <div className="userstatus">
-                <h1 className="text-xs ">today</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-        <div className="chatcomponentwindow" id="chatcomponentwindow" >
-          
-         <Chatwindow/>
+          </li>
+        </ul>
+      </nav>
+     
+    
+     
+      <div className="chatsection">
+        <div className="chatlist" id="chatlist" >
+          <div className=" chatlistbox">
+          <h1 className="">Chats</h1>
+          <div className="notify">
+          <IoIosNotificationsOutline  color="black" size={20}/>
          
+          </div>
+          
+          </div>
+          <div className="userlistbox">
+           <div className="userdetail" onClick={handleResize}>
+           <Image src={Profile} alt="profile" />
+           <h1 className="ml-2 text-sm">Alisha</h1>
+           <div className="userstatus text-xs">
+            today
+           </div>
+           </div>
+           <div className="userdetail">
+           <Image src={Profile} alt="profile" />
+           <h1 className="ml-2 text-sm">Berlin</h1>
+           <div className="userstatus text-xs">
+            today
+           </div>
+           </div>
+          </div>
         </div>
-        
+        <div className="chatwindow" id="chatwindow">
+          <Chatwindow/>
+        </div>
       </div>
-    </div>
+      </div>
+      
+      </>
+
+    
+
+    
   );
 };
 
--- src/components/chatwindow.jsx
+++ src/components/chatwindow.jsx
@@ -1,97 +1,93 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
-import { IoCallOutline } from "react-icons/io5";
-import { VscSend } from "react-icons/vsc";
-import { CiLocationOn } from "react-icons/ci";
-import { IoIosArrowBack } from "react-icons/io";
-
+import { IoCallOutline } from 'react-icons/io5';
+import { VscSend } from 'react-icons/vsc';
+import { CiLocationOn } from 'react-icons/ci';
+import { IoIosArrowBack } from 'react-icons/io';
 
 const chatwindow = () => {
   const [messages, setMessages] = useState([
-    { text: "Hi there!", sender: "receiver" },
-    { text: "Hello!", sender: "sender" },
+    { text: 'Hi there!', sender: 'receiver' },
+    { text: 'Hello!', sender: 'sender' }
   ]);
   const [inputValue, setInputValue] = useState('');
 
   const sendMessage = () => {
     if (inputValue.trim()) {
-      setMessages([...messages, { text: inputValue, sender: "sender" }]);
+      setMessages([...messages, { text: inputValue, sender: 'sender' }]);
       setInputValue('');
     }
   };
 
   const handlePhoneCall = () => {
-    const phoneNumber = "+1234567890"; 
+    const phoneNumber = '+1234567890';
     window.location.href = `tel:${phoneNumber}`;
   };
 
   const backbtn = () => {
     document.getElementById('chatlist').style.display = 'block';
-    document.getElementById('chatcomponentwindow').style.display = 'none';
+    document.getElementById('chatwindow').style.display = 'none';
   };
 
   return (
-    
     <div className="chat-window">
       <div className="chat-header">
-      <div className="chatheader">
-      <div className="user-details">
-        <div className='flex'>
-          <div className='mt-2 mx-2 chatbackbtn' onClick={backbtn}>
-          <IoIosArrowBack color='black' size={30}/>
-          </div>
+        <div className="chatheader">
+          <div className="user-details">
+            <div className="flex">
+              <div className="mt-2 mx-2 chatbackbtn" onClick={backbtn}>
+                <IoIosArrowBack color="black" size={25} />
+              </div>
 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISmJ3wr4IfIf6Y8r22sRa072YxjfXJdu1WQ&s" alt="" />
-          <div className='ml-5'>
-            <h1 className='text-xl font-light'>Olivia Rhye</h1>
-            <p className='text-xs text-pink-700 ml-1 font-light'>Online</p>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISmJ3wr4IfIf6Y8r22sRa072YxjfXJdu1WQ&s"
+                alt=""
+              />
+              <div className="ml-5 my-1">
+                <h1 className=" font-bold">Olivia Rhye</h1>
+                <p className="text-xs text-pink-700 ml-1 font-light">Online</p>
+              </div>
+            </div>
+            <div className="callbtn" onClick={handlePhoneCall}>
+              <IoCallOutline size={20} color="white" />
+              <h1 className="text-lg">call</h1>
+            </div>
           </div>
         </div>
-        <div className='callbtn'  onClick={handlePhoneCall}>
-        <IoCallOutline size={20} color='white'/>
-        <h1 className='text-lg'>call</h1>
-        </div>
-      
-      </div>
-    </div>
       </div>
       <div className="chat-body">
         {messages.map((msg, index) => (
           <div key={index} className={`message ${msg.sender}`}>
             {msg.text}
             <br />
-            <p className='msgtime'>12.00pm</p>
+            <p className="msgtime">12.00pm</p>
           </div>
         ))}
-        
       </div>
-      
+
       <div className="chat-input">
-      
-      <div className="chat-input-container">
-      <input
-         type="text"
-         value={inputValue}
-         placeholder="Type a message..."
-         onChange={(e) => setInputValue(e.target.value)}
-        className="chat-input-text"
-       
-      />
-  <div className='send-button flex'>
-    <div className='flex justify-center items-center mx-4'>
-    <CiLocationOn  color='white' size={25}/>
-    </div>
-   <div className='msgsendbtn' onClick={sendMessage}>
-    <h1>Send</h1>
-    <div>
-    <VscSend color='black' size={20} />
-    </div>
-   </div>
-  </div>
-    </div>
+        <div className="chat-input-container">
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="Type a message..."
+            onChange={(e) => setInputValue(e.target.value)}
+            className="chat-input-text"
+          />
+          <div className="send-button flex">
+            <div className="flex justify-center items-center mx-4">
+              <CiLocationOn color="white" size={20} />
+            </div>
+            <div className="msgsendbtn" onClick={sendMessage}>
+              <h1>Send</h1>
+              <div>
+                <VscSend color="black" size={20} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    
   );
 };
 
