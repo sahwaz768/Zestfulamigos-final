94f84d38bb12025d925c16e0c17f0b773c67a24f
Mukul Kathuria <mukulkathuria008@yahoo.com>
2024-11-25 11:24:04 +0530

    added packages

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { rootReducer } from './root.reducer';
+import { createWrapper } from 'next-redux-wrapper';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export const store = configureStore({
   reducer: rootReducer,
+  devTools: true,
   middleware: (getDefaultMiddleware) =>
     // eslint-disable-next-line implicit-arrow-linebreak
     getDefaultMiddleware({
@@ -12,8 +16,37 @@ export const store = configureStore({
     })
 });
 
+const makeConfiguredStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    devTools: true
+  });
+
+export const makeStore = () => {
+  const isServer = typeof window === 'undefined';
+  if (isServer) {
+    return store;
+  } else {
+    const persistConfig = {
+      key: 'nextjs',
+      whitelist: ['app'],
+      storage
+    };
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+    let store = configureStore({
+      reducer: persistedReducer,
+      devTools: process.env.NODE_ENV !== 'production'
+    });
+    store.__persistor = persistStore(store);
+    return store;
+  }
+};
+
+export const wrapper = createWrapper(makeStore);
+
 // export type storeType = ReturnType<typeof store.getState>;
 // export type storeDispatch = typeof store.dispatch;
 export const useAppDispatch = useDispatch;
 export const useAppSelector = useSelector;
+
 export const { dispatch: appDispatch, getState: appState } = store;
