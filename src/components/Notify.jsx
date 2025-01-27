bedfaab4793c9e7b6871fec69f1755aa3a9ca2fa
sahwaz768 <sahwazalam768@gmail.com>
2025-01-27 10:14:34 +0530

    noti change

--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -1,53 +1,43 @@
 'use client';
 import React, { useState } from 'react';
 import { FaRegBell } from 'react-icons/fa6';
-
+import Image from 'next/image'
+import Logo from 'src/app/companylogo.jpeg';
 
 const Notify = ({ backgroundColor = 'black', color = 'white' }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleDropdown = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    const [notifications, setNotifications] = useState([
-      {
-        id: 1,
-        avatar:
-          'https://e7.pngegg.com/pngimages/636/819/png-clipart-computer-icons-privacy-policy-admin-icon-copyright-rim.png',
-        title: 'Your financial report is overdue',
-        description:
-          'Please submit your quarterly figures for Q2 by EOB on August 15.',
-        time: 'Just now',
-        actions: []
-      },
-      {
-        id: 2,
-        avatar:
-          'https://e7.pngegg.com/pngimages/636/819/png-clipart-computer-icons-privacy-policy-admin-icon-copyright-rim.png',
-        title: 'Leave request requires your approval',
-        description:
-          'Paula Smith has requested vacation from Aug 24 – Sep 5, 2020.',
-        time: '4 minutes ago',
-        actions: ['Approve', 'Reject', 'Forward']
-      },
-      {
-        id: 3,
-        avatar:
-          'https://e7.pngegg.com/pngimages/636/819/png-clipart-computer-icons-privacy-policy-admin-icon-copyright-rim.png',
-        title: 'Blocked invoice overdue soon',
-        description:
-          'Invoice 1234678 is blocked for payment and will soon be overdue.',
-        time: '32 minutes ago',
-        actions: []
-      }
-    ]);
-  
-    const removeNotification = (id) =>
-      setNotifications(notifications.filter((notif) => notif.id !== id));
-  
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [notifications, setNotifications] = useState([
+    {
+      id: 1,
+      description:
+        'Please submit your quarterly figures for Q2 by EOB on August 15.',
+      time: 'Just now'
+    },
+    {
+      id: 2,
+      description:
+        'Paula Smith has requested vacation from Aug 24 – Sep 5, 2020.',
+      time: '4 minutes ago'
+    },
+    {
+      id: 3,
+      
+      description:
+        'Invoice 1234678 is blocked for payment and will soon be overdue.',
+      time: '32 minutes ago'
+    }
+  ]);
+
+  const removeNotification = (id) =>
+    setNotifications(notifications.filter((notif) => notif.id !== id));
+
   return (
     <div>
-       <div
+      <div
         className="bellicon"
         onClick={toggleDropdown}
         style={{ backgroundColor, color }}
@@ -68,36 +58,34 @@ const Notify = ({ backgroundColor = 'black', color = 'white' }) => {
             </svg>
           </div>
           <div className="notificatioview text-sm text-gray-900">
-            <div className="max-w-md ">
+            <div className="">
               {notifications.map((notification) => (
                 <div
                   key={notification.id}
-                  className="notification-card  p-3 flex items-start gap-3 bg-white shadow-sm rounded-md relative"
+                  className="notification-card   flex items-start  bg-white shadow-sm  relative gap-3 py-3"
                 >
                   {/* Avatar */}
-                  <img
-                    src={notification.avatar}
+                  <Image
+                    src={Logo}
                     alt="Avatar"
-                    className="w-10 h-10 rounded-full object-cover"
+                    className="w-10 h-10 rounded-full object-cover mt-1"
                   />
 
                   {/* Content */}
-                  <div className="flex-1">
-                    <div className="flex justify-between items-center">
-                      <h4 className="font-semibold text-gray-800">
-                        {notification.title}
-                      </h4>
+                  <div className="">
+                    <div className="flex justify-between items-center gap-4">
+                      <p className="text-xs text-gray-600 ">
+                        {notification.description}
+                      </p>
                       <button
                         onClick={() => removeNotification(notification.id)}
-                        className="text-blue-800 hover:text-red-500"
+                        className="text-blue-800 hover:text-red-500 crossbtn"
                       >
                         ✕
                       </button>
                     </div>
-                    <p className="text-sm text-gray-600">
-                      {notification.description}
-                    </p>
-                    <small className="text-xs text-red-700">
+
+                    <small className="text-xs text-red-700 pt-6 ">
                       {notification.time}
                     </small>
                   </div>
@@ -108,7 +96,7 @@ const Notify = ({ backgroundColor = 'black', color = 'white' }) => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Notify
+export default Notify;
